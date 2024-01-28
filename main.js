5a2734e76c9191dad318c7b527f27e6f96ff82dc
= <banzaideveloper@gmail.com>
2024-01-28 10:37:58 +0100

    Add rect to the graph

--- main.js
+++ main.js
@@ -7,36 +7,54 @@ const rawData =  fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectRe
 const data = await rawData
 
 //width: 940px height: 600px
-const height = 600;
-const width = 940
 
-const marginRight = 60;
-const marginBottom = 30;
-const marginLeft = 40;
-const marginTop = 20;
+const height = 460;
+const width = 900
 
 
+const padding = 40
+
+//data[0] == date 
+//data[1] == GDP
 const scaleY = d3.scaleLinear()
-.domain([d3.min(data, d => d[1]), d3.max(data, d => d[1])])
-.range([height - 50,0])
+.domain([d3.min(data, d => d[1]*0.00001), d3.max(data, d => d[1])])
+.range([height - padding,  padding ])
 
-const scaleX = d3.scaleLinear()
-.domain([d3.min(data, d => d[0].match(/[\d]+/gm)[0]), d3.max(data, d => d[0].match(/[\d]+/gm)[0])])
-.range([0, width -  marginRight])
 
+const scaleX = d3.scaleTime()
+.domain([d3.min(data, d => new Date(d[0])), d3.max(data, d => new Date(d[0]))])
+.range([padding, width -  padding])
 
 d3.select('#app')
 .append('svg')
 .attr('width', width)
 .attr('height', height)
 
+
+
 d3.select('svg')
 .append('g')
-.attr("transform", `translate(${marginLeft},${height - marginBottom})`)
+.attr('id', 'x-axis')
+.attr("transform", `translate(0,${height - padding})`)
 .call(d3.axisBottom(scaleX))
 
 d3.select('svg')
 .append('g')
-.attr("transform", `translate(${marginLeft},${marginTop})`)
+.attr("transform", `translate(${padding},0)`)
+.attr('id', 'y-axis')
 .call(d3.axisLeft(scaleY))
 
+//data[0] == date 
+//data[1] == GDP
+d3.select('svg')
+.selectAll('rect')
+.data(data)
+.enter()
+.append('rect')
+.attr('class', 'bar')
+.attr('data-date', (d) => d[0])
+.attr('data-gdp', (d) => d[1])
+.attr('width', 2)
+.attr('height', (d) => height - padding- scaleY(d[1]))
+.attr('x', (d) => scaleX(new Date(d[0])))
+.attr('y', (d) => scaleY(d[1]))
